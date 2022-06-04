974cc4ca80fe5501961900b6fef7499a2ecdc658
HOANGHUYHOANG <hoangfhuyhoangf@gmail.com>
2022-06-04 18:21:11 +0700

    change label

--- js/javascript.js
+++ js/javascript.js
@@ -2166,24 +2166,24 @@ let html = '<div class="question-item d-none">' +
     '<div class="answer">' +
     '<ul class="checkbox-list">' +
     '<li id="no_1">' +
-    '<label for="button1">'+
-    '<input type="radio" name="1" value="1" id=button1><span>1 - </span>'+
-    '</label>'+
+    '<label>' +
+    '<input type="radio" name="1" value="1" id="button1"><span>1 - </span>' +
+    '</label>' +
     '</li>' +
     '<li id="no_2">' +
-    '<label for="button2">'+
-    '<input type="radio" name="1" value="3" id=button2><span>2 - </span>'+
-    '</label>'+
+    '<label>' +
+    '<input type="radio" name="1" value="3" id="button2"><span>2 - </span>' +
+    '</label>' +
     '</li>' +
     '<li id="no_3">' +
-    '<label for="button3">'+
-    '<input type="radio" name="1" value="3" id=button3><span>3 - </span>'+
-    '</label>'+
+    '<label>' +
+    '<input type="radio" name="1" value="3" id="button3"><span>3 - </span>' +
+    '</label>' +
     '</li>' +
     '<li id="no_4">' +
-    '<label for="button4">'+
-    '<input type="radio" name="1" value="4" id=button4><span>4 - </span>'+
-    '</label>'+
+    '<label>' +
+    '<input type="radio" name="1" value="4" id="button4"><span>4 - </span>' +
+    '</label>' +
     '</li>' +
     '</ul>' +
     '</div>';
@@ -2256,7 +2256,7 @@ function renderQuestionPage(exam_number = 0) {
 
     let html = '<div class="left-container">' +
         '<div>' +
-        '<strong style="color: blue;">Câu Hỏi | Đề Số 1</strong>' +
+        '<strong style="color: blue;">Câu Hỏi | Đề Số ' + (exam_number + 1) + '</strong>' +
         '</div>' +
         '<ul id = "btn-list"class="btn-list">' +
         ' </ul>' +
